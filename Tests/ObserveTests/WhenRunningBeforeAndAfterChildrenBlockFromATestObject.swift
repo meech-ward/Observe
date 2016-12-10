d33be9adcae8734739dbaa0ee3e193711a2a560f
Sam Meech-Ward <sam@meech-ward.com>
2016-12-09 17:11:49 -0800

    Test the ObserveTest's runBeforeEachChild method

---
+++ Tests/ObserveTests/WhenRunningBeforeAndAfterChildrenBlockFromATestObject.swift
@@ -0,0 +1,44 @@
+//
+//  WhenRunningBeforeAndAfterChildrenBlockFromATestObject.swift
+//  Observe
+//
+//  Created by Sam Meech-Ward on 2016-12-09.
+//
+//
+
+@testable import Observe
+import XCTest
+
+class WhenRunningBeforeAndAfterChildrenBlockFromATestObject: XCTestCase {
+
+    var observeTest: ObserveTest!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        observeTest = ObserveTest()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func test_runBeforeEachChild() {
+        
+    }
+    
+    func test_runBeforeEachChild_runsTheClosure() {
+        var run = false
+        observeTest.beforeEachChild = {
+            run = true
+        }
+        
+        observeTest.runBeforeEachChild()
+        
+        XCTAssertTrue(run)
+    }
+}
+func test_popNextChild() {
+    
+}
