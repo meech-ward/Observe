f4ece367069543f7cc8725807fc24cf76352cf2c
Sam Meech-Ward <sam@meech-ward.com>
2018-02-24 14:55:16 -0800

    Add an cocoa touch framework for the source files

---
+++ iOS/Observe/Observe.h
@@ -0,0 +1,19 @@
+//
+//  Observe.h
+//  Observe
+//
+//  Created by Sam Meech-Ward on 2018-02-24.
+//  Copyright © 2018 lighthouse-labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Observe.
+FOUNDATION_EXPORT double ObserveVersionNumber;
+
+//! Project version string for Observe.
+FOUNDATION_EXPORT const unsigned char ObserveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observe/PublicHeader.h>
+
+
